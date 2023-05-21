2341592beab70723ed30eae772348a3fcd6cd51a
Ignacio A. Rumbo <71301204+ignaciorumbodesk@users.noreply.github.com>
2023-05-21 06:05:53 -0300

    Tickets Html & Js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+
+
+function calcPrecioFinal() {
+    console.log("ingreso a calcular precio");
+
+    //En cantEnt almacenamos la cantidad de entradas que ingreso el usuario
+    let cantEnt=document.getElementById("cantEntradas").value;
+    console.log("cant: " + cantEnt);
+
+    //En porcDesc almacenamos el porcentaje de descuento que selecciona el usuario
+    let porcDesc=document.getElementById("porcDescuento").value;
+    console.log("porcDescuento: " + porcDesc);
+
+}
