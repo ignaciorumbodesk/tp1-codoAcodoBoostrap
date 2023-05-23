e64f8d817c01c888016bc30938591aac0df00d95
Ignacio NoteBook <nacho.r.85@hotmail.com>
2023-05-23 16:00:11 -0300

    formulario de tickets. primer parte

--- scripts.js
+++ scripts.js
@@ -10,7 +10,17 @@ function calcPrecioFinal() {
     console.log("cant: " + cantEnt);
 
     //En porcDesc almacenamos el porcentaje de descuento que selecciona el usuario
-    let porcDesc=document.getElementById("porcDescuento").value;
-    console.log("porcDescuento: " + porcDesc);
+    let desc=document.getElementById("porcDescuento").value;
+    console.log("desc: " + desc);
 
+    //Obetenemos el decuento que hay q realizar
+    let descTotal = (cantEnt * 1000) * desc / 100;
+    console.log("descTotal" + descTotal);
+
+    //Obtenemos el descuento total del precio total
+    let precioFin = (cantEnt * 1000) - descTotal;
+    console.log(precioFin);
+
+    //Coloco en html el valor de las entradas - el descuento
+    document.getElementById("precioFinal").value=precioFin;
 }
